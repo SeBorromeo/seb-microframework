db05bbe9604fabc84ad0bc93a18bab86657f8b93
Sebastian Borromeo <uwg3xs@virginia.edu>
2026-01-25 04:08:10 -0500

    testQueryParsing, testContentTypeParsing, more header tests

--- tests/Unit/Http/RequestTest.php
+++ tests/Unit/Http/RequestTest.php
@@ -5,7 +5,7 @@ use Sebastian\MicroFramework\Http\Request;
 
 class RequestTest extends TestCase {
     public function testMagicProperties(): void {
-        $req = Request::createFromGlobals();
+        $req = new Request();
 
         $req->userId = 1;
         $this->assertSame(1, $req->userId);
@@ -21,21 +21,48 @@ class RequestTest extends TestCase {
         $req->method = 'This shouldnt be possible';
     }
 
-    public function testQueryParsing(): void {
+    public function testContentTypeParsing(): void {
+        $serverParams = [
+            'CONTENT_TYPE' => 'application/x-www-form-urlencoded; charset= 8',
+        ];
 
+        $req = new Request($serverParams);
+
+        $this->assertSame('application/x-www-form-urlencoded', $req->contentType());
+    }
+
+    public function testQueryParsing(): void {
+        $serverParams = [
+            'QUERY_STRING' => 'q=solar&page=2&sort=asc',
+        ];
+
+        $req = new Request($serverParams);
+
+        $this->assertSame([
+            'q' => 'solar',
+            'page' => '2',
+            'sort' => 'asc'
+        ], $req->query());
     }
 
     public function testHeaderParsing(): void {
-        $server = [
+        $serverParams = [
             'HTTP_X_CUSTOM_HEADER' => 'custom-value',
             'CONTENT_TYPE' => 'application/json',
         ];
 
-        $req = new Request($server);
+        $req = new Request($serverParams);
 
         $this->assertSame('custom-value', $req->header('X-CUSTOM-HEADER'));
         $this->assertSame('custom-value', $req->header('x-custom-header'));
         $this->assertSame('application/json', $req->header('Content-Type'));
         $this->assertNull($req->header('Non-Existent'));
+
+        $this->assertEquals(['x-custom-header' => 'custom-value', 'content-type' => 'application/json'], $req->headers());
+
+        $this->assertTrue($req->hasHeader('X-CUSTOM-HEADER'));
+        $this->assertTrue($req->hasHeader('x-custom-header'));
+        $this->assertTrue($req->hasHeader('Content-Type'));
+        $this->assertFalse($req->hasHeader('Non-Existent'));
     }
 }
