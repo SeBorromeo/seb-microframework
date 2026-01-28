6824e79e4ba64cb48ab7331531d8601c822658ee
Sebastian Borromeo <uwg3xs@virginia.edu>
2026-01-28 00:22:02 -0500

    Application config and default view path

--- src/Application.php
+++ src/Application.php
@@ -1,5 +1,18 @@
 <?php namespace Sebastian\MicroFramework;
 
 final class Application {
-    
+    protected array $config = [];
+
+    public function set(string $key, mixed $value): void {
+        $this->config[$key] = $value;
+    }
+
+    public function get(string $key, mixed $default = null): mixed {
+        return $this->config[$key] ?? $default;
+    }
+
+    public function __construct() {
+        $this->set('view path', getcwd());
+    }
+
 }
