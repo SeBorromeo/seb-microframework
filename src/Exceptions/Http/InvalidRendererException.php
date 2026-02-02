cbac78461fb389489bff0507b4f3648e50fa1eac
Sebastian Borromeo <uwg3xs@virginia.edu>
2026-02-01 22:11:26 -0500

    Different error msg for null engine in InvalidRendererException

--- src/Exceptions/Http/InvalidRendererException.php
+++ src/Exceptions/Http/InvalidRendererException.php
@@ -3,10 +3,13 @@
 class InvalidRendererException extends \LogicException
 {
     public function __construct(
-        string $engine,
+        string|null $engine,
         int $code = 0,
         ?\Throwable $previous = null
     ) {
-        parent::__construct("Renderer engine {$engine} is not supported.", $code, $previous);
+        if ($engine)
+            parent::__construct("Renderer engine {$engine} is not supported.", $code, $previous);
+        else
+            parent::__construct("Renderer engine must not be null. Set using \$app->set('view engine', {engine}). Options include: php and pug", $code, $previous);
     }
 }
