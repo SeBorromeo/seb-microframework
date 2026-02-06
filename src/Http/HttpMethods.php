8ed28a0209d15cca52e742ce53bb2eaa6bbaeaa5
Sebastian Borromeo <uwg3xs@virginia.edu>
2026-02-06 16:00:08 -0500

    HttpMethods class, encapsulates all http methods and static helper functions

---
+++ src/Http/HttpMethods.php
@@ -0,0 +1,19 @@
+<?php namespace Sebastian\MicroFramework\Http;
+
+final class HttpMethods {
+    public const ALL = [
+        'CHECKOUT', 'COPY', 'DELETE', 'GET', 'HEAD', 'LOCK', 'MERGE',
+        'MKACTIVITY', 'MKCOL', 'MOVE', 'MSEARCH', 'NOTIFY',
+        'OPTIONS', 'PATCH', 'POST', 'PURGE', 'PUT',
+        'REPORT', 'SEARCH', 'SUBSCRIBE', 'TRACE',
+        'UNLOCK', 'UNSUBSCRIBE'
+    ];
+
+    public static function all(): array {
+        return array_map('strtolower', self::ALL);
+    }
+
+    public static function isValid(string $method): bool {
+        return in_array(strtoupper($method), self::ALL, true);
+    }
+}
