95999e8fb2440d57be6cb2afe8b4c6d3137196be
Sebastian Borromeo <uwg3xs@virginia.edu>
2026-02-06 16:22:27 -0500

    Regex lib class normalized further taking string literal and flags and turning into valid regex

--- src/Routing/Lib/Regex.php
+++ src/Routing/Lib/Regex.php
@@ -1,13 +1,29 @@
 <?php namespace Sebastian\MicroFramework\Routing\Lib;
 
 class Regex {
+    public const DEFAULT_DELIMITER = '#';
+    public const ALLOWED_FLAGS = ['i','m','s','x','u'];
+
     private string $pattern;
 
-    public function __construct(string $pattern) {
-        if (@preg_match($pattern, '') === false) 
-            throw new \InvalidArgumentException("Invalid regex pattern: $pattern");
+    public function __construct(string $pattern, array $flags = []) {
+        $pattern = str_replace(self::DEFAULT_DELIMITER, '\\' . self::DEFAULT_DELIMITER, $pattern);
 
-        $this->pattern = $pattern;
+        foreach ($flags as $f) {
+            if (!in_array($f, self::ALLOWED_FLAGS, true)) {
+                throw new \InvalidArgumentException("Invalid regex flag: $f");
+            }
+        }
+
+        $flagStr = implode('', $flags);
+
+        $fullPattern = self::DEFAULT_DELIMITER . $pattern . self::DEFAULT_DELIMITER . $flagStr;
+
+        if (@preg_match($fullPattern, '') === false) {
+            throw new \InvalidArgumentException("Invalid regex pattern: $fullPattern");
+        }
+
+        $this->pattern = $fullPattern;
     }
 
     public function __toString(): string {
