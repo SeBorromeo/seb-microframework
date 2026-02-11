d84a85b1fa5d1493ea867e25818f91d6d061de9b
Sebastian Borromeo <uwg3xs@virginia.edu>
2026-02-10 19:37:50 -0500

    Added PHPDoc type hints for parseGroup test

--- tests/Unit/Routing/Lib/PathUtilsTest.php
+++ tests/Unit/Routing/Lib/PathUtilsTest.php
@@ -33,41 +33,57 @@ class PathUtilsTest extends TestCase {
     public function testParseParameter(): void {
         $result = PathUtils::parse('/users/:id');
 
-        $this->assertCount(2, $result->tokens);
-        $this->assertInstanceOf(Text::class, $result->tokens[0]);
-        $this->assertSame('/users/', $result->tokens[0]->text);
-        $this->assertInstanceOf(Parameter::class, $result->tokens[1]);
-        $this->assertSame('id', $result->tokens[1]->name);
+        /** @var Token[] */
+        $tokens = $result->tokens;
+
+        $this->assertCount(2, $tokens);
+        $this->assertInstanceOf(Text::class, $tokens[0]);
+        $this->assertSame('/users/', $tokens[0]->value);
+        $this->assertInstanceOf(Parameter::class, $tokens[1]);
+        $this->assertSame('id', $tokens[1]->name);
     }
 
     public function testParseWildcard(): void {
         $result = PathUtils::parse('/files/*filepath');
 
-        $this->assertCount(2, $result->tokens);
-        $this->assertInstanceOf(Text::class, $result->tokens[0]);
-        $this->assertSame('/files/', $result->tokens[0]->value);
-        $this->assertInstanceOf(Wildcard::class, $result->tokens[1]);
-        $this->assertSame('filepath', $result->tokens[1]->name);
+        /** @var Token[] */
+        $tokens = $result->tokens;
+
+        $this->assertCount(2, $tokens);
+        $this->assertInstanceOf(Text::class, $tokens[0]);
+        $this->assertSame('/files/', $tokens[0]->value);
+        $this->assertInstanceOf(Wildcard::class, $tokens[1]);
+        $this->assertSame('filepath', $tokens[1]->name);
     }
 
     public function testParseGroup(): void {
         $result = PathUtils::parse('/posts{/:year{/:month}}');
 
-        $this->assertCount(2, $result->tokens);
-        $this->assertInstanceOf(Text::class, $result->tokens[0]);
-        $this->assertSame('/posts', $result->tokens[0]->value);
-        $this->assertInstanceOf(Group::class, $result->tokens[1]);
+        /** @var Token[] */
+        $tokens = $result->tokens;
+        $this->assertCount(2, $tokens);
+        $this->assertInstanceOf(Text::class, $tokens[0]);
+        $this->assertSame('/posts', $tokens[0]->value);
+        $this->assertInstanceOf(Group::class, $tokens[1]);
 
-        $group1 = $result->tokens[1];
+        /** @var Group */
+        $group1 = $tokens[1];
         $this->assertCount(3, $group1->tokens);
         $this->assertInstanceOf(Parameter::class, $group1->tokens[1]);
-        $this->assertSame('year', $group1->tokens[1]->name);
+
+        /** @var Parameter */
+        $yearParam = $group1->tokens[1];
+        $this->assertSame('year', $yearParam->name);
         $this->assertInstanceOf(Group::class, $group1->tokens[2]);
 
+        /** @var Group */
         $group2 = $group1->tokens[2];
         $this->assertCount(2, $group2->tokens);
-        $this->assertInstanceOf(Parameter::class, $group2->tokens[1]);
-        $this->assertSame('month', $group2->tokens[1]->name);
+
+        /** @var Parameter */
+        $monthParam = $group2->tokens[1];
+        $this->assertInstanceOf(Parameter::class, $monthParam);
+        $this->assertSame('month', $monthParam->name);
     }
 
     /* ---------- Match ---------- */
