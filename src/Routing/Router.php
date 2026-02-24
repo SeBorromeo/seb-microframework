7b4e24905dac6de7a414104f6c7e75b98aa20e67
Sebastian Borromeo <uwg3xs@virginia.edu>
2026-02-23 22:00:28 -0500

    Router invoke so obj can be used as callable, param and http methods

--- src/Routing/Router.php
+++ src/Routing/Router.php
@@ -1,5 +1,10 @@
 <?php namespace SeBorromeo\SebMicroframework\Routing;
 
+use SeBorromeo\SebMicroframework\Http\Request;
+use SeBorromeo\SebMicroframework\Http\Response;
+use SeBorromeo\SebMicroframework\Http\HttpMethod;
+use SeBorromeo\PathToRegex\Regex;
+
 class Router {
     /** @var Layer[] */
     private array $stack = [];
@@ -17,5 +22,47 @@ class Router {
         $this->caseSensitive = $options['case sensitive'] ?? false;
         $this->mergeParams = $options['merge params'] ?? false;
         $this->strict = $options['strict'] ?? false;
+    }
+
+    /**
+     * Magic method to turn Router into a callable.
+     */
+    public function __invoke(Request $request, Response $response, callable $next) {
+        $this->handle($request, $response, $next);
+    }
+
+    /**
+     * Map given param $name to given callback.
+     */
+    public function param(string $name, callable $fn): Router {
+        if (!isset($this->params[$name])) {
+            $this->params[$name] = [];
+        }
+
+        $this->params[$name][] = $fn;
+
+        return $this;
+    }
+    /* ---------- HTTP Methods ---------- */
+
+    /**
+     * Magic method to handle dynamic HTTP method calls.
+     * 
+     * @throws BadMethodCallException
+     * @throws InvalidArgumentException
+     */
+    public function __call(string $method, array $args): Router {
+        if (is_null(HttpMethod::fromString($method)))
+            throw new \BadMethodCallException("Method $method does not exist");
+        
+        if (count($args) === 0) 
+            throw new \InvalidArgumentException('First argument $path is required');
+
+        if (!is_string($args[0]))
+            throw new \InvalidArgumentException('First argument $path must be of type string');
+
+        $route = $this->route($args[0]);
+        $route->{$method}(...array_slice($args, 1));
+        return $this;
     }
 }
